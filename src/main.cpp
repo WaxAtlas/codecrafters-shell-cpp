6298b31f9022e3d2a60bf1f13e7a49e9918216f9
Watson Carawon <carawonw@gmail.com>
2026-06-23 17:15:47 -0400

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,38 @@
 #include <string>
 
 int main() {
+  std::string builtins[3]{"echo", "exit", "type"};
+
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  while(true) { 
+  while (true) {
     std::cout << "$ ";
-  
+
     std::string user_input;
     std::getline(std::cin, user_input);
     std::stringstream ss(user_input);
+
+    std::string command;
+    std::getline(ss, command, ' ');
+
+    std::string token;
+    if (command == "exit") {
+      return 0;
+    } else if (command == "type") {
+      std::getline(ss, token, ' ');
+      if (std::find(std::begin(builtins), std::end(builtins), token) !=
+          std::end(builtins)) {
+        std::cout << token << " is a shell builtin" << std::endl;
+      } else {
+        std::cout << token << ": command not found" << std::endl;
+      }
+    } else if (command == "echo") {
+      std::cout << user_input.substr(5) << std::endl;
+    } else {
+      std::cout << user_input << ": command not found" << std::endl;
+    }
 
     // std::string token;
     // while (std::getline(ss, token, ' ')) {
@@ -28,15 +50,15 @@ int main() {
     //   }
     // }
 
-    std::string command = user_input.substr(0, user_input.find(" "));
-    if (command == "exit") {
-      return 0;
-    }
-    else if (command == "echo") {
-      std::cout << user_input.substr(5) << std::endl;
-    }
-    else {
-      std::cout << user_input << ": command not found" << std::endl;
-    }
+    // std::string command = user_input.substr(0, user_input.find(" "));
+    // if (command == "exit") {
+    //   return 0;
+    // } else if (command == "type") {
+
+    // } else if (command == "echo") {
+    //   std::cout << user_input.substr(5) << std::endl;
+    // } else {
+    //   std::cout << user_input << ": command not found" << std::endl;
+    // }
   }
 }
